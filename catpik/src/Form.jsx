8250bedabea8efaa08c6776fa5b2ad33acf343ae
Mount-Blanc <mountblanc100@gmail.com>
2023-08-26 10:27:05 -0700

    added form component

---
+++ catpik/src/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form>
+    <label>Username</label>
+    <input placeholder='Enter Username'/>
+
+    <label>Password</label>
+    <input placeholder='Enter Password'/>
+  </form>
+  )
+}
+
+export default Form
