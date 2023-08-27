9b9cf2a907c238d7cc01cf950a225a678cd79ac1
Mount-Blanc <mountblanc100@gmail.com>
2023-08-27 16:20:51 -0700

    added login get request to authroutes

--- catpik/src/backend/index.js
+++ catpik/src/backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const port = 3000;
+const authRoute= require('./routes/authRoutes')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
 
 
+app.route('/login',authRoute)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
