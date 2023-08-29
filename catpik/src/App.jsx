2d749db469c1e0985fecf30dcef21b17d4fcfeeb
Mount-Blanc <mountblanc100@gmail.com>
2023-08-28 18:38:26 -0700

    moved all components to component folder

--- catpik/src/App.jsx
+++ catpik/src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import Login from './Login';
+import Login from './components/Login';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
-import ErrorPage from './ErrorPage';
-import Register from "./Register";
+import ErrorPage from './components/ErrorPage';
+import Register from "./components/Register";
 import './App.css'
 
 function App() {
   return(
+    <>
+    <h1>CatPik</h1>
      <Routes>
         <Route path="/" errorElement={<ErrorPage/>} element={<Login/>} />,
         <Route path="/register" errorElement={<ErrorPage/>} element={<Register/>} />
 
       </Routes>
+      </>
   );
 }
 
