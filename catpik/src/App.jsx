15560aecc1622b8e5cda9149d56dd52f7cfb0b2b
Mount-Blanc <mountblanc100@gmail.com>
2023-08-31 15:55:12 -0700

    added root component

--- catpik/src/App.jsx
+++ catpik/src/App.jsx
@@ -3,14 +3,17 @@ import Login from './components/Login';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import ErrorPage from './components/ErrorPage';
 import Register from "./components/Register";
+import Root from './components/Root';
 import './App.css'
+
 
 function App() {
   return(
     <>
     <h1>CatPik</h1>
      <Routes>
-        <Route path="/" errorElement={<ErrorPage/>} element={<Login/>} />,
+      <Route path='/' errorElement={<ErrorPage/>} element={<Root/>} />
+        <Route path="/Login" errorElement={<ErrorPage/>} element={<Login/>} />,
         <Route path="/register" errorElement={<ErrorPage/>} element={<Register/>} />
 
       </Routes>
