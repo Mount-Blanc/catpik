595b51f223f03da47a2fe794103adf19c827503b
Mount-Blanc <mountblanc100@gmail.com>
2023-09-01 09:22:30 -0700

    added username and password keyvalue pairs to users array

--- catpik/src/backend/controller/authController.js
+++ catpik/src/backend/controller/authController.js
@@ -4,16 +4,15 @@ const users = [];
 
 exports.postRegister = (req, res, next) => {
   const { username, password } = req.body;
-  console.log(password)
-  const userCredentials = password;
-  bcrypt.hash(password, 10, (err, hash) => {
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
     if (err) {
       console.error("Error hashing password:", err);
       return;
-    }
-    console.log(hash)
+    }  
+    users.push({username,password:hashedPassword});
+
+    console.log(users)
   });
-  users.push(userCredentials);
   res.json({ message: "User Registered Successfully" });
   next();
 };
