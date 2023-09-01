9065bb2fcfa5b48a57338224d767958e207a0336
Mount-Blanc <mountblanc100@gmail.com>
2023-08-31 18:10:55 -0700

    added style to root

--- catpik/src/components/Root.jsx
+++ catpik/src/components/Root.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import './Root.css'
+import "./Root.css";
 import { Link } from "react-router-dom";
 
 function Root() {
   return (
-    <div>
-      <h1>Welcome to Catpik</h1>
-      <Link className="root-button" to='./Login'>Login</Link>    
-      <Link className="root-button" to="./register">Register</Link>
+    <div className="background-container">
+      <div className="welcome-container">
+        <h1>Welcome to Catpik</h1>
+        <Link className="root-button" to="./Login">
+          Login
+        </Link>
+        <Link className="root-button" to="./register">
+          Register
+        </Link>
+      </div>
     </div>
   );
 }
