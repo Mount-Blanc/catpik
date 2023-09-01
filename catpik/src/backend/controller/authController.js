1b8e3d0d136b4f95867776f5bab6dd206cabd854
Mount-Blanc <mountblanc100@gmail.com>
2023-09-01 09:15:55 -0700

    added bycrypt to encrypt passwords

--- catpik/src/backend/controller/authController.js
+++ catpik/src/backend/controller/authController.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const users= [];
+const express = require("express");
+const bcrypt = require("bcrypt");
+const users = [];
 
-exports.postRegister = (req,res,next) => {
-    const { username, password} = req.body
-    users.push({username,password})
-    console.log(users)
-    res.json({message:"User Registered Successfully"})
-    next()
-}
-
+exports.postRegister = (req, res, next) => {
+  const { username, password } = req.body;
+  console.log(password)
+  const userCredentials = password;
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.error("Error hashing password:", err);
+      return;
+    }
+    console.log(hash)
+  });
+  users.push(userCredentials);
+  res.json({ message: "User Registered Successfully" });
+  next();
+};
