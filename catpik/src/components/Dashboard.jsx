a11a5369061a58f4b03365ce7c579a6720faff9a
Mount-Blanc <mountblanc100@gmail.com>
2023-09-01 18:50:05 -0700

    added logout feature

--- catpik/src/components/Dashboard.jsx
+++ catpik/src/components/Dashboard.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 function Dashboard() {
+  const navigate = useNavigate();
 
 
+  const handleLogout  =async () => {
 
+    try {
+      const response = await axios.get('http://localhost:3000/logout');
+      console.log('Response:', response);
+      navigate('/login',{replace:true})
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    
+
+
+  };
 
   return (
     <>
     <div>Dashboard</div>
-    <button>logout</button>
+    <button onClick={handleLogout}>logout</button>
     </>
   )
 }
