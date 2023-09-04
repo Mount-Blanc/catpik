4a5e8584037b5eb78f5362a673df8b661a9a6242
Mount-Blanc <mountblanc100@gmail.com>
2023-09-04 10:25:59 -0700

    reorganized index.js code

--- catpik/src/backend/index.js
+++ catpik/src/backend/index.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
+const helmet = require('helmet');
+const authRoute = require('./routes/authRoutes');
+const authController = require('./controller/authController');
+
 const app = express();
 const port = 3000;
-const authRoute= require('./routes/authRoutes')
-const cors = require('cors'); // Import the 'cors' package
-const authController = require('./controller/authController')
 
-
+// Middleware
 app.use(cors());
-app.use(express.json())
+app.use(helmet());
+app.use(express.json());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"],
+      imgSrc: ['self'],
+      connectSrc: ["'self'"],
+      fontSrc: ['self'],
+      objectSrc: ["'none'"],
+    },
+  })
+);
+app.use(helmet.xContentTypeOptions());
+app.use(helmet.xFrameOptions());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+// Session middleware
 app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
 
-
+// Routes
 app.use('/', authRoute);
-app.post('/register',authController.postRegister)
-app.get('/logout', authController.logout)
-app.post('/login', authController.login)
-
+app.post('/register', authController.postRegister);
+app.get('/logout', authController.logout);
+app.post('/login', authController.login);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
